47c20662b79297ac36ceca4a3cfe648172b5dcc5
Czuee Morey <cmo@wega-it.com>
2019-08-22 11:47:03 +0200

    Shiny app dev

--- candy.R
+++ candy.R
@@ -1,32 +1,77 @@
 library(shiny)
 ui <- fluidPage(
+   sidebarLayout(
+      sidebarPanel(
+         fileInput("file1", "Choose CSV File",
+                   accept = c(
+                      "text/csv",
+                      "text/comma-separated-values,text/plain",
+                      ".csv")
+      ),
       numericInput(inputId = "choc", 
                    label = "Chocolate", 
                    value = 1, min = 0, max = 1, step = 1),
-      radioButtons(inputId = "fru", 
+      numericInput(inputId = "fruit", 
                    label = "Fruity", 
-                   choices = c("Yes", "No")) ,
-      radioButtons(inputId = "caram", 
+                   value = 0, min = 0, max = 1, step = 1),
+      numericInput(inputId = "caram", 
                    label = "Caramel", 
-                   choices = c("Yes", "No")) ,
-      radioButtons(inputId = "pa", 
-                   label = "Peanutyalmondy", 
-                   choices = c("Yes", "No")) ,
+                   value = 1, min = 0, max = 1, step = 1),
+      numericInput(inputId = "pa", 
+                   label = "PeanutyAlmondy", 
+                   value = 1, min = 0, max = 1, step = 1),
+      numericInput(inputId = "noug", 
+                   label = "Nougat", 
+                   value = 0, min = 0, max = 1, step = 1),
+      numericInput(inputId = "crwf", 
+                   label = "CrispedRiceWafer", 
+                   value = 1, min = 0, max = 1, step = 1),
+      numericInput(inputId = "hard", 
+                   label = "Hard", 
+                   value = 0, min = 0, max = 1, step = 1),
+      numericInput(inputId = "bar", 
+                   label = "Bar", 
+                   value = 1, min = 0, max = 1, step = 1),
+      numericInput(inputId = "plb", 
+                   label = "Pluribus", 
+                   value = 1, min = 0, max = 1, step = 1),
       sliderInput(inputId = "sugar", 
                   label = "Choose sugar percentile", 
                   value = 0.6, min = 0, max = 1),
       sliderInput(inputId = "price", 
                   label = "Choose price percentile", 
-                  value = 0.6, min = 0, max = 1),
-      textOutput(outputId = "score") 
+                  value = 0.6, min = 0, max = 1)
+      ),
+      mainPanel(
+         plotOutput("hist"),
+         verbatimTextOutput(outputId = "score")          
+      )
+   )
 )
 
 server <- function(input, output) {
-      output$score <- renderText({0.3183 + 0.19*input$choc + 
-                  0.143*input$sugar - 0.095*input$price})
+      y <- reactive({
+         0.3183 + 0.19*input$choc + 
+            0.114*input$fruit + 0.044*input$caram + 0.102*input$pa -
+            0.017*input$noug + 0.127*input$crwf - 0.062*input$hard + 
+            0.027*input$bar - 0.017*input$plb +
+            0.143*input$sugar - 0.095*input$price
+      })
+      
+      output$hist <- renderPlot({
+         inFile <- input$file1
+         
+         if (is.null(inFile))
+            return(NULL)
+         
+         dat <- read.csv(inFile$datapath, header = TRUE)
+         
+         title("Histogram of original data")
+         hist(dat$winpercent)
+         abline(v=y(), col="red")
+      })
+      output$score <- renderPrint({y()})
 }
-#output$score <- 0.318 + 0.193*input$choc + 0.114*input$fru + 
-#0.044*input$caram + 0.102*input$pa + 0.143*input$sugar 
-#- 0.095*input$price
+
 
 shinyApp(ui = ui, server = server)
